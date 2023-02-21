c6727d8610919253e673e26155887a912e7b33a3
Jin Sakura <sakura0419gen@icloud.com>
2023-02-21 15:45:09 +0900

    [replace]dressUp design

--- lib/logic/navigate.dart
+++ lib/logic/navigate.dart
@@ -63,10 +63,6 @@ class _NavigateState extends State<Navigate> {
       ),
       body: Stack(
         children: [
-          Container(
-            alignment: Alignment.bottomCenter,
-            child: Image.asset('assets/parts/bottom_navigation_bar.png'),
-          ),
           FutureBuilder<Map<String, String?>>(
               future: _sharedPreferencesLogic.fetchID(),
               builder: (context, snapshot) {
